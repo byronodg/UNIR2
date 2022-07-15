953c8bb9b743da07b1032f14025b2f3eb12e0ff5
byronodg <100391866+byronodg@users.noreply.github.com>
2022-07-15 18:10:12 -0500

    Add files via upload

---
+++ EXAMEN/index.js
@@ -0,0 +1,10 @@
+console.log("holita");
+console.log(11111);
+function salida(){
+            console.log('hola');
+}
+function sumar(a,b){
+            console.log(a+b*2);
+}
+var restar= function(a,b){return a-b;}
+        
